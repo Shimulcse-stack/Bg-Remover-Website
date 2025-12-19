c00e8b9cc567f3e80bc57fbdd5ea35b6784ef617
Sibly Sadik Shimul <shimul.cse28@gmail.com>
2025-12-20 04:23:39 +0600

    Add files via upload

---
+++ server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Required for SharedArrayBuffer (cross-origin isolation)
+app.use((req, res, next) => {
+  res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+  res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+  next();
+});
+
+app.use(express.static(__dirname));
+
+app.listen(8080, () => {
+  console.log("Open http://127.0.0.1:8080");
+});
